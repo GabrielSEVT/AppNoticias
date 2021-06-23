bbcbc5c6190ec72ab4174c9c56e12106a5ea687f
GabrilSEVT <gabriel.evangelista14@etec.sp.gov.br>
2021-06-23 09:28:55 -0300

    Validação de login

--- app/src/main/java/com/example/appnoticias/MainActivity.java
+++ app/src/main/java/com/example/appnoticias/MainActivity.java
@@ -6,15 +6,39 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnConvidado;
+    Button btnLogin;
+    TextView campoEmail;
+    TextView campoSenha;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = (Button)findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                campoEmail = (TextView)findViewById(R.id.campoEmail);
+                campoSenha = (TextView)findViewById(R.id.campoSenha);
+                String email = campoEmail.getText().toString().trim();
+                String senha = campoSenha.getText().toString().trim();
+                if(email.equals("cotidiano@gmail.com") && senha.equals("admin")){
+                    Intent intent = new Intent (MainActivity.this, TelaRecentes.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(MainActivity.this, "Login ou senha inválida",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        });
 
         btnConvidado = (Button)findViewById(R.id.btnConvidado);
         btnConvidado.setOnClickListener(new View.OnClickListener(){
